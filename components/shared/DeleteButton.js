2a341ab460ed78dcf0cf06140221857922bb726d
mbwakeley <44876447+mbwakeley@users.noreply.github.com>
2020-03-07 10:37:51 -0700

    gradient color buttones, now work on the header

--- components/shared/DeleteButton.js
+++ components/shared/DeleteButton.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function DeleteButton({ text, onPress }) {
   return (
     <TouchableOpacity onPress={onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{text}</Text>
+      <View>
+        <LinearGradient
+          colors={["#f85032", "#f16f5c", "#f02f17", "#e73827"]}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>{text}</Text>
+        </LinearGradient>
       </View>
     </TouchableOpacity>
   );
@@ -16,11 +22,10 @@ const styles = StyleSheet.create({
     width: 100,
     borderRadius: 8,
     paddingVertical: 14,
-    paddingHorizontal: 10,
-    backgroundColor: "#cb4154"
+    paddingHorizontal: 10
   },
   buttonText: {
-    color: "white",
+    color: "#373737",
     fontWeight: "bold",
     textTransform: "uppercase",
     fontSize: 16,
